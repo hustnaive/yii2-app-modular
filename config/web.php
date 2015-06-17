aaf8c07ce6ca9b846f9c19ae0183f5144cca5c57
fangliang <fangl@mysoft.com.cn>
2015-06-17 11:04:15 +0800

    remove vendor

--- config/web.php
+++ config/web.php
@@ -3,9 +3,10 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'basic',
+    'id' => 'modular',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'demo/site/index',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
